e03bc701e797f045fe82312dac2aaa882b09b364
Dhakal-Kushal <dhkushaldh@gmail.com>
2026-04-08 19:49:34 -0400

    init commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,15 @@ import RegisterPage from "./pages/RegisterPage.tsx";
 
 import LoginPage from './pages/LoginPage.tsx';
 import CardPage from './pages/CardPage.tsx';
+import NotePage from './pages/NotePage.tsx';
 
 function App()
 {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<LoginPage />} />
+				<Route path="/" element={<NotePage />} />
+				<Route path="/login" element={<LoginPage />} />
 				<Route path="/register" element={<RegisterPage />} />
 				<Route path="/cards" element={<CardPage />} />
 			</Routes>
