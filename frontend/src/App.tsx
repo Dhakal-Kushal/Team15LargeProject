a319ecaabac1a3c2635fa4dd2d605d5e5e3dfd82
Attila Nagy <Niky20062@gmail.com>
2026-04-08 17:45:56 -0400

    No changes yet

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import RegisterPage from "./pages/RegisterPage.tsx";
+
+import LoginPage from './pages/LoginPage.tsx';
+import CardPage from './pages/CardPage.tsx';
+
+function App()
+{
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
+				<Route path="/cards" element={<CardPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
