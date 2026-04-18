15d0916ff4fe4b6b33b01344e51ac54ad1dfc605
Sammy Dabbas <dabbassammy@gmail.com>
2026-04-18 17:13:56 -0400

    use https and nginx api proxy no so port 5000

--- frontend/src/components/Path.ts
+++ frontend/src/components/Path.ts
@@ -1,10 +1,10 @@
 const app_name = 'team15study.com';
 
 export function buildPath(route: string) {
-    // Vite uses import.meta.env.DEV to check if you are running locally
+    // Vite uses import.meta.env.DEV to check if running locally
     if (import.meta.env.DEV) {
         return 'http://localhost:5000/' + route;
     } else {
-        return 'http://' + app_name + ':5000/' + route;
+        return 'https://' + app_name + '/' + route;
     }
 }
